64502dc8f36f6bf1c3f10e0355a5a79f91860304
vishalmet <vishalmet003@gmail.com>
2025-08-17 15:38:06 +0530

    Add Radix UI button component and asset icons

---
+++ components/Assets.tsx
@@ -0,0 +1,21 @@
+import Aptos from "../app/assets/icons/aptos.png";
+import Arbitrum from "../app/assets/icons/arbitrum.png";
+import Avalanche from "../app/assets/icons/avalanche.png";
+import Bsc from "../app/assets/icons/bsc.png";
+import Ethereum from "../app/assets/icons/ethereum.png";
+import Polygon from "../app/assets/icons/polygon.png";
+import Solana from "../app/assets/icons/solana.png";
+import Sui from "../app/assets/icons/sui.png";
+import ZkSync from "../app/assets/icons/zksync.png";
+
+export const Assets = {
+    Aptos,
+    Arbitrum,
+    Avalanche,
+    Bsc,
+    Ethereum,
+    Polygon,
+    Solana,
+    Sui,
+    ZkSync,
+};
